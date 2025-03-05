6a3a226b1d3d6523425bdfd4d29ac5a14e92b287
karpushinds600 <karpushinds@evolenta.ru>
2025-03-05 09:01:44 +0300

    Задание 3. Массивы, циклы, условные операторы, математические функции, (списки и мапы по желанию)

--- Main.java
+++ Main.java
@@ -1,58 +1,25 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите 3 числа \n" );
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-
-        boolean found = false;
-
-        if (a % 5 == 0) {
-            System.out.println("a=" + a);
-            found = true;
+        Scanner in = new Scanner(System.in);
+        int[] array = new int[20];
+        int[] check = new int[15];
+        for (int i = 0; i < 20; ++i) {
+            array[i] = (int) Math.ceil(Math.random() * 15D);
+            if (i == 0)
+                System.out.printf("[%d, ", array[i]);
+            else if (i == 19)
+                System.out.printf("%d]", array[i]);
+            else
+                System.out.printf("%d, ", array[i]);
+            ++check[array[i] - 1];
         }
-        if (b % 5 == 0) {
-            System.out.println("b=" + b);
-            found = true;
-        }
-        if (c % 5 == 0) {
-            System.out.println("c=" + c);
-            found = true;
-        }
-
-        if (!found) {
-            System.out.println("нет значений, кратных 5");
-        }
-
-        int result1 = a / b;
-        System.out.println("Результат целочисленного деления a на b: " + result1);
-
-        double result2 = (double) a / b;
-        System.out.println("Результат деления на a и b: " + result2);
-
-        int result3 = (int)Math.ceil(result2);
-        System.out.println("Результат деления на a и b с округлением в большую сторону: " + result3);
-
-        int result4 = (int)Math.floor(result2);
-        System.out.println("Результат деления на a и b с округлением в меньшую сторону: " + result4);
-
-        long result5 = Math.floorDiv(Math.round(result2), 1L);
-        System.out.println("Результат деления на a и b с математическим округлением: " + result5);
-
-        int result6 = b % c;
-        System.out.println("Остаток от деления b на c: " + result6);
-
-        int result7 = Math.min (a, b);
-        System.out.println("Наименьшее значение из a и b: " + result7);
-
-        int result8 = Math.max (b, c);
-        System.out.println("Наибольшее значение из b и c: " + result8);
-
-
+        System.out.println();
+        for (int i = 0; i < 15; ++i)
+            if (check[i] > 1)
+                System.out.printf("Число '%d' встречается %d раза \n", i + 1, check[i]);
+        in.close();
     }
-
-
 }
